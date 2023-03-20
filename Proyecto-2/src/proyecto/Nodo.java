0ff2abccb87a8fc0f1b37d0eb2efde42c0810909
BeaCardozo <113352853+BeaCardozo@users.noreply.github.com>
2023-03-20 17:39:44 -0400

    Update Nodo.java

--- Proyecto-2/src/proyecto/Nodo.java
+++ Proyecto-2/src/proyecto/Nodo.java
@@ -5,43 +5,43 @@
 package proyecto;
 
 /**
- *
- * @author andre
+ * Esta clase, define funciones y atributos necesarios para el uso del Nodo de la estructura.
+ * @author: andre
+ * @version: 22/03/2023
  */
 public class Nodo {
     
     private String key;
     private String index;
 
+    /**
+    * Constructor para la clase Nodo
+    */
     public Nodo(String key) {
         this.key = key;
         this.index = "";
-    }
+    } //Cierre del constructor
 
     /**
      * @return the key
      */
     public String getKey() {
         return key;
-    }
+    } //Cierre del la función
 
     /**
      * @return the index
      */
     public String getIndex() {
         return index;
-    }
+    } //Cierre del la función
 
     /**
      * @param index the index to set
      */
     public void setIndex(String index) {
         this.index += index+",";
-    }
+    } //Cierre del la función
+
     
-    
-    
-    
-    
-    
-}
+} //Cierre de la clase
